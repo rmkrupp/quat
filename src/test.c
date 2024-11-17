cbee691a3b598613b88c8c588abf14249c43feac
Emery Parks <emeryparks@protonmail.com>
2024-11-16 23:46:07 -0500

    some basic tests

--- src/test.c
+++ src/test.c
@@ -18,9 +18,246 @@
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
 #include "quat.h"
+#include <math.h>
+#include <stdio.h>
+#include <stdlib.h>
+
+size_t unexpected = 0;
+double accuracy = 1e-12;
+
+char ** string_pool = NULL;
+size_t string_pool_size = 0;
+
+void free_string_pool()
+{
+    for (size_t i = 0; i < string_pool_size; i++) {
+        free(string_pool[i]);
+    }
+    free(string_pool);
+    string_pool = NULL;
+    string_pool_size = 0;
+}
+
+const char * strof_q(const struct quaternion * q)
+{
+    char * buffer = malloc(1024);
+    snprintf(buffer, 1024, "<%f %f %f %f>", q->x, q->y, q->z, q->w);
+    string_pool = realloc(
+            string_pool, sizeof(*string_pool) * (string_pool_size + 1));
+    string_pool[string_pool_size++] = buffer;
+    return buffer;
+}
+
+const char * strof_v2(const struct vec2 * u)
+{
+    char * buffer = malloc(1024);
+    snprintf(buffer, 1024, "<%f %f>", u->x, u->y);
+    string_pool = realloc(
+            string_pool, sizeof(*string_pool) * (string_pool_size + 1));
+    string_pool[string_pool_size++] = buffer;
+    return buffer;
+}
+
+const char * strof_v3(const struct vec3 * u)
+{
+    char * buffer = malloc(1024);
+    snprintf(buffer, 1024, "<%f %f %f>", u->x, u->y, u->z);
+    string_pool = realloc(
+            string_pool, sizeof(*string_pool) * (string_pool_size + 1));
+    string_pool[string_pool_size++] = buffer;
+    return buffer;
+}
+
+const char * strof_v4(const struct vec4 * u)
+{
+    char * buffer = malloc(1024);
+    snprintf(buffer, 1024, "<%f %f %f %f>", u->x, u->y, u->z, u->w);
+    string_pool = realloc(
+            string_pool, sizeof(*string_pool) * (string_pool_size + 1));
+    string_pool[string_pool_size++] = buffer;
+    return buffer;
+}
+
+const char * strof_m(const struct matrix * m)
+{
+    char * buffer = malloc(1024);
+    snprintf(
+            buffer,
+            1024,
+            "[[%f %f %f %f]\n"
+            " [%f %f %f %f]\n"
+            " [%f %f %f %f]\n"
+            " [%f %f %f %f]]",
+            m->matrix[0],
+            m->matrix[1],
+            m->matrix[2],
+            m->matrix[3],
+            m->matrix[4],
+            m->matrix[5],
+            m->matrix[6],
+            m->matrix[7],
+            m->matrix[8],
+            m->matrix[9],
+            m->matrix[10],
+            m->matrix[11],
+            m->matrix[12],
+            m->matrix[13],
+            m->matrix[14],
+            m->matrix[15]
+        );
+    string_pool = realloc(
+            string_pool, sizeof(*string_pool) * (string_pool_size + 1));
+    string_pool[string_pool_size++] = buffer;
+    return buffer;
+}
+
+void expect_vec3(const struct vec3 * a, const struct vec3 * b)
+{
+    if (fabs(a->x - b->x) > accuracy ||
+        fabs(a->y - b->y) > accuracy ||
+        fabs(a->z - b->z) > accuracy) {
+        printf("UNEXPECTED: %s != %s\n", strof_v3(a), strof_v3(b));
+        unexpected++;
+    }
+}
 
 int main(int argc, char ** argv)
 {
     (void)argc;
     (void)argv;
+
+    struct quaternion a;
+    struct matrix m;
+
+    {
+        struct vec4 u = { .x = 0.0, .y = 0.0, .z = 1.0, .w = 1.0 };
+        printf("input = %s\n", strof_v4(&u));
+        quaternion_from_axis_angle(&a, 1.0, 0.0, 0.0, M_PI / 2);
+        printf("rotate by x axis, pi / 2 (90°)\n");
+        printf("quaternion = %s\n", strof_q(&a));
+        quaternion_matrix(&m, &a);
+        printf("as matrix =\n%s\n", strof_m(&m));
+        matrix_multiply_vec4(&u, &m, &u);
+        printf("as vec4 = %s\n", strof_v4(&u));
+        struct vec3 v = { .x = u.x / u.w, .y = u.y / u.w, .z = u.z / u.w };
+        printf("output = %s\n", strof_v3(&v));
+        expect_vec3(&v, &(struct vec3){0, -1, 0});
+    }
+    printf("\n");
+
+    {
+        struct vec4 u = { .x = 0.0, .y = 0.0, .z = 1.0, .w = 1.0 };
+        printf("input = %s\n", strof_v4(&u));
+        quaternion_from_axis_angle(&a, 1.0, 0.0, 0.0, -M_PI / 2);
+        printf("rotate by x axis, -pi / 2 (-90°)\n");
+        printf("quaternion = %s\n", strof_q(&a));
+        quaternion_matrix(&m, &a);
+        printf("as matrix =\n%s\n", strof_m(&m));
+        matrix_multiply_vec4(&u, &m, &u);
+        printf("as vec4 = %s\n", strof_v4(&u));
+        struct vec3 v = { .x = u.x / u.w, .y = u.y / u.w, .z = u.z / u.w };
+        printf("output = %s\n", strof_v3(&v));
+        expect_vec3(&v, &(struct vec3){0, 1, 0});
+    }
+    printf("\n");
+
+    {
+        struct vec4 u = { .x = 0.0, .y = 0.0, .z = 1.0, .w = 1.0 };
+        printf("input = %s\n", strof_v4(&u));
+        quaternion_from_axis_angle(&a, 0.0, 1.0, 0.0, M_PI / 2);
+        printf("rotate by y axis, pi / 2 (90°)\n");
+        printf("quaternion = %s\n", strof_q(&a));
+        quaternion_matrix(&m, &a);
+        printf("as matrix =\n%s\n", strof_m(&m));
+        matrix_multiply_vec4(&u, &m, &u);
+        printf("as vec4 = %s\n", strof_v4(&u));
+        struct vec3 v = { .x = u.x / u.w, .y = u.y / u.w, .z = u.z / u.w };
+        printf("output = %s\n", strof_v3(&v));
+        expect_vec3(&v, &(struct vec3){1, 0, 0});
+    }
+    printf("\n");
+
+    {
+        struct vec4 u = { .x = 0.0, .y = 0.0, .z = 1.0, .w = 1.0 };
+        printf("input = %s\n", strof_v4(&u));
+        quaternion_from_axis_angle(&a, 0.0, 1.0, 0.0, -M_PI / 2);
+        printf("rotate by y axis, -pi / 2 (-90°)\n");
+        printf("quaternion = %s\n", strof_q(&a));
+        quaternion_matrix(&m, &a);
+        printf("as matrix =\n%s\n", strof_m(&m));
+        matrix_multiply_vec4(&u, &m, &u);
+        printf("as vec4 = %s\n", strof_v4(&u));
+        struct vec3 v = { .x = u.x / u.w, .y = u.y / u.w, .z = u.z / u.w };
+        printf("output = %s\n", strof_v3(&v));
+        expect_vec3(&v, &(struct vec3){-1, 0, 0});
+    }
+    printf("\n");
+
+    {
+        struct vec4 u = { .x = 0.0, .y = 0.0, .z = 1.0, .w = 1.0 };
+        printf("input = %s\n", strof_v4(&u));
+        quaternion_from_axis_angle(&a, 0.0, 0.0, 1.0, -M_PI / 2);
+        printf("rotate by z axis, -pi / 2 (-90°)\n");
+        printf("quaternion = %s\n", strof_q(&a));
+        quaternion_matrix(&m, &a);
+        printf("as matrix =\n%s\n", strof_m(&m));
+        matrix_multiply_vec4(&u, &m, &u);
+        printf("as vec4 = %s\n", strof_v4(&u));
+        struct vec3 v = { .x = u.x / u.w, .y = u.y / u.w, .z = u.z / u.w };
+        printf("output = %s\n", strof_v3(&v));
+        expect_vec3(&v, &(struct vec3){0, 0, 1});
+    }
+    printf("\n");
+
+    {
+        struct vec4 u = { .x = 0.0, .y = 0.0, .z = 1.0, .w = 1.0 };
+        printf("input = %s\n", strof_v4(&u));
+        quaternion_from_axis_angle(&a, 0.0, 0.0, 1.0, M_PI / 2);
+        printf("rotate by z axis, pi / 2 (90°)\n");
+        printf("quaternion = %s\n", strof_q(&a));
+        quaternion_matrix(&m, &a);
+        printf("as matrix =\n%s\n", strof_m(&m));
+        matrix_multiply_vec4(&u, &m, &u);
+        printf("as vec4 = %s\n", strof_v4(&u));
+        struct vec3 v = { .x = u.x / u.w, .y = u.y / u.w, .z = u.z / u.w };
+        printf("output = %s\n", strof_v3(&v));
+        expect_vec3(&v, &(struct vec3){0, 0, 1});
+    }
+    printf("\n");
+
+    {
+        struct vec4 u = { .x = 0.0, .y = 0.0, .z = 1.0, .w = 1.0 };
+        printf("input = %s\n", strof_v4(&u));
+        quaternion_from_axis_angle(&a, 1.0, 0.0, 0.0, M_PI);
+        printf("rotate by x axis, pi (180°)\n");
+        printf("quaternion = %s\n", strof_q(&a));
+        quaternion_matrix(&m, &a);
+        printf("as matrix =\n%s\n", strof_m(&m));
+        matrix_multiply_vec4(&u, &m, &u);
+        printf("as vec4 = %s\n", strof_v4(&u));
+        struct vec3 v = { .x = u.x / u.w, .y = u.y / u.w, .z = u.z / u.w };
+        printf("output = %s\n", strof_v3(&v));
+        expect_vec3(&v, &(struct vec3){0, 0, -1});
+    }
+    printf("\n");
+
+    {
+        struct vec4 u = { .x = 0.0, .y = 0.0, .z = 1.0, .w = 1.0 };
+        printf("input = %s\n", strof_v4(&u));
+        quaternion_from_axis_angle(&a, 0.0, 1.0, 0.0, M_PI);
+        printf("rotate by y axis, pi (180°)\n");
+        printf("quaternion = %s\n", strof_q(&a));
+        quaternion_matrix(&m, &a);
+        printf("as matrix =\n%s\n", strof_m(&m));
+        matrix_multiply_vec4(&u, &m, &u);
+        printf("as vec4 = %s\n", strof_v4(&u));
+        struct vec3 v = { .x = u.x / u.w, .y = u.y / u.w, .z = u.z / u.w };
+        printf("output = %s\n", strof_v3(&v));
+        expect_vec3(&v, &(struct vec3){0, 0, -1});
+    }
+    printf("\n");
+
+    printf("%zu unexpected\n", unexpected);
+
+    free_string_pool();
 }
